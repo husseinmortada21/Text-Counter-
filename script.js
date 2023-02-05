3c6fad9f89123d518f0756255f0631dc1a43b5e4
husseinmortada21 <110199564+husseinmortada21@users.noreply.github.com>
2023-02-05 14:04:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ let stats = document.querySelector('.status');
 	let lines = document.getElementById('lines');
 	let symbols = document.getElementById('symbols');
 		function count() {
-		if(text.value.length === 0){ // can be easily done using CSS, check CSS for that
+		if(text.value.length === 0){ 
 			stats.style.display = "none";
 		}
 		else{
 			stats.style.display = "block";
-			char.innerHTML = text.value.length + " Characters" //char
-		words.innerHTML = text.value.trim().split(/\s+/).length + " Words"; //words
+			char.innerHTML = text.value.length + " Characters" // Char Length
+		words.innerHTML = text.value.trim().split(/\s+/).length + " Words"; //words Length
 		lines.innerHTML = text.value.split("\n").length + " Lines"; //lines
-		symbols.innerHTML = text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length + " Symbols"
+		symbols.innerHTML = text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length + " Symbols" // Symbols Length
 		}
 	}
 text.addEventListener("input", count);
+//You may also insert this JS Script inside the HTML directly .
+// Created by Hussein M.
